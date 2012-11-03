74256ba777eb689b8f5e859c099775efed3e8f29
DimaBes <moto1252012@gmail.com>
2012-11-03 17:00:31 +0200

    fd

---
+++ bl2_1_4/1_4.cpp
@@ -0,0 +1,43 @@
+﻿/* 
+ * File:   1_4.cpp
+ * Author: Home
+ *
+ * Created on 27 Октябрь 2012 г., 19:25
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int gcd (int a,int b)
+{
+    if (!a) return b;
+    else return gcd(b%a,a);
+}
+/*123
+
+ */
+int main() 
+{
+    int n,res;
+    cout<<"input n (2<=n<=100):"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"input a[0]=";
+    cin>>a[0];
+    cout<<"input a[1]=";
+    cin>>a[1];
+    res=fabs(a[1]-a[0]);
+    
+    for (int i=2;i<n;i++)
+    {cout<<"input a["<<i<<"]=";
+        cin>>a[i];
+    res=gcd(res,fabs(a[i]-a[i-1]));
+   
+    }
+    
+    cout<<"gcd="<<res<<endl;     
+    return 0;
+}
+
