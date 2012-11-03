183ca73f1feb77116751c4c9a504ef270d7258dc
trialbes1 <moto1252009@rambler.ru>
2012-11-03 20:21:24 +0200

    sdgdf

---
+++ qq/bl2_1_4/1_4.cpp
@@ -0,0 +1,91 @@
+﻿<<<<<<< HEAD
+﻿/* 
+ * File:   1_4.cpp
+ * Author: Homeafsdfsdf
+ *dfsdfdsfdsffsdfsd
+ * Created on 27 Октябрь 2012 г., 19:25
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+int gcd (int a,int b)
+{
+    if (!a) return b;
+    else return gcd(b%a,a);
+}
+/*123
+
+ */
+int main() 
+{
+    int n,res;
+    cout<<"input n (2<=n<=100):"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"input a[0]=";
+    cin>>a[0];
+    cout<<"input a[1]=";
+    cin>>a[1];
+    res=fabs(a[1]-a[0]);
+    
+    for (int i=2;i<n;i++)
+    {cout<<"input a["<<i<<"]=";
+        cin>>a[i];
+    res=gcd(res,fabs(a[i]-a[i-1]));
+   
+    }
+    
+    cout<<"gcd="<<res<<endl;     
+    return 0;
+}
+
+=======
+﻿/* 
+ * File:   1_4.cpp
+ * Author: Home
+ *ываыаыв
+ * Created on 27 Октябрь 2012 г., 19:25
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+int gcd (int a,int b)
+{
+    if (!a) return b;
+    else return gcd(b%a,a);
+}
+/*123
+
+ */
+int main() 
+{
+    int n,res;
+    cout<<"input n (2<=n<=100):"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"input a[0]=";
+    cin>>a[0];
+    cout<<"input a[1]=";
+    cin>>a[1];
+    res=fabs(a[1]-a[0]);
+    
+    for (int i=2;i<n;i++)
+    {cout<<"input a["<<i<<"]=";
+        cin>>a[i];
+    res=gcd(res,fabs(a[i]-a[i-1]));
+   
+    }
+    
+    cout<<"gcd="<<res<<endl;     
+    return 0;
+}
+
+>>>>>>> 7d2d769a08bfd7633c9180dac082f1e2413e65ab
